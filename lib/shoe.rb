a7081a51988b5c51023a31b482e50970225bd811
OyakMasu21 <akzaki17@gmail.com>
2023-02-09 19:11:19 +0300

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def initialize( brand="Nike",color="red", size = 9.5, material="suede", condition="tattered" )
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+        
+    end
+
+    def cobble
+    puts "Your shoe is as good as new!"
+    if @condition ="old" 
+        return @condition = "new"
+        end
+    end
+
+end
+
+shoe = Shoe.new()
+shoe.cobble
+
+binding.pry
